5e5fde66f58ebf8916f8c3dc29a30866ebc4c79e
sprengerjo <sprengerj22@gmail.com>
2017-06-22 21:13:24 +0200

    add 3 different implementations at once
    - go up to n iterations shows performance of different solutions

--- app/js/fizzbuzz.js
+++ app/js/fizzbuzz.js
@@ -4,8 +4,8 @@ const R = require('ramda');
 
 class FizzBuzz {
 
-    static upTo(n) {
-        const replaceBlanksWithNumber = (fb, idx) => R.isEmpty(fb) ? "" + R.inc(idx) : fb;
+    static upToStringZip(n) {
+        const replaceBlanksWithNumber = (fb, idx) => R.isEmpty(fb) ? '' + R.inc(idx) : fb;
         const mapIndexed = R.addIndex(R.map);
         const repFlat = (s, n) => R.compose(
             R.take(n),
@@ -13,8 +13,33 @@ class FizzBuzz {
             R.repeat(s))(n);
 
         return mapIndexed(replaceBlanksWithNumber, R.zipWith(R.concat,
-                repFlat(["", "", "fizz"], n),
-                repFlat(["", "", "", "", "buzz"], n)));
+            repFlat(['', '', 'fizz'], n),
+            repFlat(['', '', '', '', 'buzz'], n)));
+    }
+
+    static upToLookUp(n) {
+        const s = R.toString
+        const f = R.always('fizz');
+        const b = R.always('buzz');
+        const fb = R.always(f() + b());
+        const funs = [s, s, f, s, b, f, s, s, f, b, s, f, s, s, fb];
+
+        const selectFun = n => R.nth(R.modulo(R.dec(n), 15), funs);
+        const fizzBuzzify = (n) => R.call(selectFun(n), n);
+
+        return R.map(fizzBuzzify, R.range(1, R.inc(n)));
+    }
+
+    static upToConditional(n) {
+        const fbMod = mod => i => i % mod === 0;
+        const fb = R.cond([
+            [fbMod(15), R.always('fizzbuzz')],
+            [fbMod(5), R.always('buzz')],
+            [fbMod(3), R.always('fizz')],
+            [R.T, i => '' + i]
+        ]);
+
+        return R.map(fb, R.range(1, R.inc(n)));
     }
 }
 
--- test/unit/test_fizzbuzz.js
+++ test/unit/test_fizzbuzz.js
@@ -1,14 +1,41 @@
 'use strict';
 
 const should = require('should');
-
+const R = require('ramda');
 const FizzBuzz = require('./../../app/js/fizzbuzz');
 
 describe('fizz buzz', () => {
 
-    it("should return empty array for empty cell", () => {
-        should(FizzBuzz.upTo(16)).eql(["1", "2", "fizz", "4", "buzz", "fizz", "7", "8", "fizz",
-            "buzz", "11", "fizz", "13", "14", "fizzbuzz", "16"]);
+    const n = 1000000;
+    const functions = [FizzBuzz.upToStringZip, FizzBuzz.upToLookUp, FizzBuzz.upToConditional];
+
+    const assertFizzBuzz = (n, expected) => R.pipe(
+        R.map(fun => R.call(fun, n)),
+        R.forEach(actual => should(actual).eql(expected)))
+        (functions);
+
+    it('should assert fizz buzz up to ...', () => {
+        assertFizzBuzz(1, ["1"]);
+        assertFizzBuzz(3, ["1", '2', 'fizz']);
+        assertFizzBuzz(5, ["1", '2', 'fizz', '4', 'buzz']);
+        assertFizzBuzz(17, ["1", '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz', 'buzz', '11', 'fizz', '13', '14', 'fizzbuzz', '16', '17']);
     });
-  
+
+    it(`upToStringZip '${n}' iterations`, () => {
+        const actual = FizzBuzz.upToStringZip(n);
+        should(actual.length).eql(n);
+        should(R.last(actual)).eql('buzz');
+    });
+
+    it(`upToLookUp '${n}' iterations`, () => {
+        const actual = FizzBuzz.upToLookUp(n);
+        should(actual.length).eql(n);
+        should(R.last(actual)).eql('buzz');
+    });
+
+    it(`upToConditional '${n}' iterations`, () => {
+        const actual = FizzBuzz.upToConditional(n);
+        should(actual.length).eql(n);
+        should(R.last(actual)).eql('buzz');
+    });
 });
